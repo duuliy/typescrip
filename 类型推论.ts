9839d8390111dc3b30dac6011168adae166973ca
zhongyuhan <715181149@qq.com>
2019-07-25 09:25:04 +0800

    feat: infer

--- 类型推论.ts
+++ 类型推论.ts
@@ -24,3 +24,18 @@ window.onmousedown = function(mouseEvent) {
 window.onmousedown = function(mouseEvent: any) {
     console.log(mouseEvent.button);  //<- Now, no error is given
 };
+
+
+//infer 最早出现在此 PR 中，表示在 extends 条件语句中待推断的类型变量。
+
+type ParamType<T> = T extends (param: infer P) => any ? P : T;
+
+  interface User {
+    name: string;
+    age: number;
+  }
+  
+  type Func = (user: User) => void;
+  
+  type Param = ParamType<Func>; // Param = User
+  type AA = ParamType<string>; // string
