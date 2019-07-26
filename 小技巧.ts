d97a572d400ca9a24c651c5a2a7366618c3c8982
zhongyuhan <715181149@qq.com>
2019-07-26 14:30:55 +0800

    feat: 小技巧

---
+++ 小技巧.ts
@@ -0,0 +1,7 @@
+//有个工具函数 ts.forEachChild，可以用来访问 AST 任一节点的所有子节点。
+//SyntaxKind 被定义为一个常量枚举
+// ts.createScanner  扫描器
+// parseSourceFile 解析器   
+//ts.bindSourceFile 绑定器
+//ts.mergeSymbolTable  检查器
+//emitSourceFile 发射器
