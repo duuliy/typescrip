76e0e39592e27c06d6e9a78271cb083575e9349c
zhongyuhan <715181149@qq.com>
2019-08-13 17:13:36 +0800

    feat: 类 接口的泛型

--- 泛型.ts
+++ 泛型.ts
@@ -48,9 +48,40 @@ loggingIdentity2(3);  // Error, number doesn't have a .length property
 
 loggingIdentity2({length: 10, value: 3});
 
-
 //泛型不可使用为函数返回类型
 
 
+//2 泛型类
+class GetMin<T>{
+    arr:T[]=[];
+    add(ele:T){
+        this.arr.push(ele);
+    }
+   min():T{
+      var min=this.arr[0];
+      this.arr.forEach(function (value) {
+         if(value<min){
+             min=value;
+         }
+      });
+      return min;
+   }
+}
 
-//为什么不要在泛型函数中写 typeof T、new T, 或者 instanceof T？
+ 
+/**
+ * 3 泛型函数接口
+ */
+
+interface GenericIdentityFn {
+    <T>(arg: T): T;
+}
+function identity22<T>(arg: T): T {
+    return arg;
+}
+let myIdentity2: GenericIdentityFn = identity22;
+
+
+
+
+//为什么不要在泛型函数中写 typeof T、new T, 或者 instanceof T？ 编译器区分不出来
