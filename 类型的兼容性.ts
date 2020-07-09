82aefa702f0088b0930a20e75daf2c5bad8dfa32
zhongyuhan <715181149@qq.com>
2020-07-09 10:49:42 +0800

    1

--- 类型的兼容性.ts
+++ 类型的兼容性.ts
@@ -64,7 +64,7 @@ invokeLater([1, 2], (x, y) => console.log(x + ', ' + y));
 invokeLater([1, 2], (x?, y?) => console.log(x + ', ' + y));
 
 
-//枚举类型与数字类型兼容，并且数字类型与枚举类型兼容。不同枚举类型之间是不兼容的
+//枚举类型与数字类型兼容，并且数字类型与枚举类型兼容。不同枚举类型之间是不兼容的.
 enum Status { Ready, Waiting };
 enum Color { Red, Blue, Green };
 
