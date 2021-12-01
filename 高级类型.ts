cdca7e90c772c801bc6067ca0976c71f1f0cac3c
duuliy <715181149@qq.com>
2021-12-01 15:10:08 +0800

    feat: & 交集

--- 高级类型.ts
+++ 高级类型.ts
@@ -2,12 +2,18 @@
 
 //**********************************高级类型 ******************************** */
 
-//交叉类型（&）取并集，联合类型（|）用 或 理解
+//交叉类型（&）取交集，联合类型（|）用 或 理解
 
 //交叉类型
 //交叉类型是将多个类型合并为一个类型。 
 //这让我们可以把现有的多种类型叠加到一起成为一种类型，它包含了所需的所有类型的特性。
 //我们大多是在混入（mixins）或其它不适合典型面向对象模型的地方看到交叉类型的使用。
+
+type Env1 = 'prod' | 'test' | 'dev';
+type Env2 = 'prod' | 'boe' | 'ppe';
+
+type EnvUnion = Env1 | Env2; // 'prod' | 'test' | 'dev' | 'boe' | 'ppe'
+type EnvInter = Env1 & Env2; // 'prod'
 
 function extend<T, U>(first: T, second: U): T & U {
     let result = <T & U>{};
